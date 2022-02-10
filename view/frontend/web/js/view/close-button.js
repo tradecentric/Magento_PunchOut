81d9559ad69ee063177884277616ea60e406c359
Sergey Poprygin <sergei.p@ecomitize.com>
2022-02-10 14:49:27 +0200

    PUN-22

--- view/frontend/web/js/view/close-button.js
+++ view/frontend/web/js/view/close-button.js
@@ -1,10 +1,24 @@
 define([
     'jquery',
+    'underscore',
+    'Magento_Customer/js/customer-data',
     'Punchout2Go_Punchout/js/model/destroy-session',
-], function($, destroySession) {
+], function($, _, customerData, destroySession) {
     'use strict'
 
+    let punchoutSession = customerData.get('punchout-session');
+    function toggleLink(elem, punchoutId) {
+        if (_.isEmpty(punchoutId)) {
+            $(elem).hide();
+        } else {
+            $(elem).show();
+        }
+    }
     return function (config, elem) {
+        toggleLink(elem, punchoutSession().punchoutId);
+        punchoutSession.subscribe(function (data) {
+            toggleLink(elem, data.punchoutId);
+        });
         $(elem).click(function() {
             destroySession();
             $.mage.redirect(config.redirectUrl);
