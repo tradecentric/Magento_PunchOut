0abb8f07cf51c24ae7bccdc3f11f1277cac96100
Sergey Poprygin <sergei.p@ecomitize.com>
2022-11-22 18:29:24 +0200

    PUN-126

---
+++ Model/Transfer/QuoteItemTransferData/Fields/Currency.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Punchout2Go\Punchout\Model\Transfer\QuoteItemTransferData\Fields;
+
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Magento\Quote\Api\Data\CartItemInterface;
+use Punchout2Go\Punchout\Model\Transfer\QuoteItemTransferData\QuoteItemRelatedDataHandlerInterface;
+
+/**
+ * Currency
+ *
+ * Class Currency
+ * @package Punchout2Go\Punchout\Model\Transfer\QuoteItemTransferData
+ */
+class Currency implements QuoteItemRelatedDataHandlerInterface
+{
+    /**
+     * @param CartItemInterface $product
+     * @param $storeId
+     * @return array
+     */
+    public function handle(CartItemInterface $product, $storeId): array
+    {
+        $store = $product->getQuote()->getStore();
+        return ['currency' => $store->getCurrentCurrencyCode()];
+    }
+}
