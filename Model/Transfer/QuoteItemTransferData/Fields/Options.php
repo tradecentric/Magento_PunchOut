9a78b9087eb49eb831c34ad1c2417be84506f832
Sergey Poprygin <sergei.p@ecomitize.com>
2023-02-22 23:52:46 +0200

    PUN-143

---
+++ Model/Transfer/QuoteItemTransferData/Fields/Options.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Punchout2Go\Punchout\Model\Transfer\QuoteItemTransferData\Fields;
+
+use Magento\Quote\Api\Data\CartItemInterface;
+use Punchout2Go\Punchout\Model\Transfer\QuoteItemTransferData\QuoteItemRelatedDataHandlerInterface;
+use Magento\Catalog\Helper\Product\Configuration as MagentoConfiguration;
+
+class Options implements QuoteItemRelatedDataHandlerInterface
+{
+    /**
+     * @var MagentoConfiguration
+     */
+    private $configuration;
+
+    /**
+     * @param MagentoConfiguration $configuration
+     */
+    public function __construct(MagentoConfiguration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    /**
+     * Get cart item options
+     *
+     * @param CartItemInterface $cartItem
+     * @param $storeId
+     *
+     * @return array
+     */
+    public function handle(CartItemInterface $cartItem, $storeId): array
+    {
+        $options = $this->configuration->getOptions($cartItem);
+        if (!$options) {
+            return [];
+        }
+        $values = array_map(function(array $option) {
+            return $option['value'] ?? '';
+        }, $options);
+        return ['options' => $values];
+    }
+}
