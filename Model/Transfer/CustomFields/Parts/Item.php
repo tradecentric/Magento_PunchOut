f9509042e0576486a655722494b8b0aef082cedf
dan bosler <dan.bosler@tradecentric.com>
2024-10-25 10:34:46 -0400

    CN-393 item mapping

--- Model/Transfer/CustomFields/Parts/Item.php
+++ Model/Transfer/CustomFields/Parts/Item.php
@@ -20,17 +20,9 @@ class Item implements CartItemPartInterface
     public function handle(CartItemInterface $product, string $path): string
     {
         $returnValue = '';
-<<<<<<< HEAD
         $productData = $product->getData($path);
         if (isset($productData)) {
            $returnValue = $product->getData($path);
-=======
-		$productData = $product->getData($path);
-        if (isset($productData)) {
-//            $attribute = $product->getResource()->getAttribute($path);
-//            $returnValue = $attribute->getFrontend()->getValue($product);
-			$returnValue = $product->getData($path);
->>>>>>> b7376b6 (CN-393 item mapping initial)
         }
         return $returnValue;
     }
