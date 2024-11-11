f57a0dd7d0a7cff2933859fa2b901c206746edb7
Dan Bosler <149419849+tc-dan-bosler@users.noreply.github.com>
2024-11-11 14:26:05 -0500

    CN-393 item mapping (#4)
    * CN-test additional_data
    
    * testing
    
    * CN-test testing
    
    * CN-test testing
    
    * CN-test testing
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test update
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test remove cartItem
    
    * CN-test fix
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-test
    
    * CN-393 update item mapping
    
    * CN-393 item mapping
    
    * CN-393 item mapping
    
    * CN-393 item mapping initial
    
    CN-393 update item mapping
    
    * CN-393 item mapping
    
    * CN-393 update item mapping
    
    * CN-393 item mapping
    
    * CN-393 item mapping
    
    * CN-393 item mapping initial
    
    CN-393 update item mapping
    
    * CN-393 item mapping
    
    * CN-393_item_mapping PR fixes
    
    * CN-393_item_mapping update
    
    * CN-393_item_mapping update
    
    * CN-393 update Item
    
    * CN-393 removed spaces
    
    ---------
    
    Co-authored-by: Jay Grayson <87324282+p2g-jay-grayson@users.noreply.github.com>

---
+++ Model/Transfer/CustomFields/Parts/Item.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Punchout2Go\Punchout\Model\Transfer\CustomFields\Parts;
+
+use Magento\Quote\Api\Data\CartItemInterface;
+use Punchout2Go\Punchout\Model\Transfer\CustomFields\CartItemPartInterface;
+
+/**
+ * Class Item
+ * @package Punchout2Go\Punchout\Model\Transfer\CustomFields\Parts
+ */
+class Item implements CartItemPartInterface
+{
+    /**
+     * @param CartItemInterface $product
+     * @param string $path
+     * @return string
+     */
+    public function handle(CartItemInterface $product, string $path): string
+    {
+        return $product->getData($path) ?? '';
+    }
+}
