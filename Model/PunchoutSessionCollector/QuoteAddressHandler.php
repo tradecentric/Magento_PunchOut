d59a48a4ef457134a79b48759db1a3a18ec2d870
dan bosler <dan.bosler@tradecentric.com>
2025-08-19 09:21:19 -0500

    CN-746_attach_ship_to_cart

--- Model/PunchoutSessionCollector/QuoteAddressHandler.php
+++ Model/PunchoutSessionCollector/QuoteAddressHandler.php
@@ -80,14 +80,14 @@ class QuoteAddressHandler implements EntityHandlerInterface
 			if ($addressData) {
 				$this->logger->log('Customer Shipping Address');
 				$this->logger->log(print_r($addressData, true));
-				$this->updateSessionQuoteAddress($object, $addressData, $type);
+				$this->updateSessionQuoteAddress($object, $addressData, 'shipping');
 			}
 
 			$addressData = $this->getCustomerAddressData($customerAddresses, 'billing');
 			if ($addressData) {
 				$this->logger->log('Customer Shipping Billing');
 				$this->logger->log(print_r($addressData, true));
-				$this->updateSessionQuoteAddress($object, $addressData, $type);
+				$this->updateSessionQuoteAddress($object, $addressData, 'billing');
 			}
 		}
         $this->logger->log('Quote Address Setup Complete');
