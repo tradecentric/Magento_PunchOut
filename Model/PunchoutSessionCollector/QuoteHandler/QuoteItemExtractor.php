bfdb1e23a1d9ad41ca3821cde39858bb20e6416c
emanuela.arsene <emanuela.arsene@tradecentric.com>
2026-05-10 19:09:50 +0300

    fix: [CN-1162] make QuoteItemExtractor tolerate inactive quotes

--- Model/PunchoutSessionCollector/QuoteHandler/QuoteItemExtractor.php
+++ Model/PunchoutSessionCollector/QuoteHandler/QuoteItemExtractor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Punchout2Go\Punchout\Model\PunchoutSessionCollector\QuoteHandler;
 
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\CartRepositoryInterface;
 
 /**
  * Class QuoteItemExtractor
@@ -17,17 +18,17 @@ class QuoteItemExtractor
     protected $items = [];
 
     /**
-     * @var \Magento\Quote\Api\CartItemRepositoryInterface
+     * @var CartRepositoryInterface
      */
-    protected $cartItemRepository;
+    protected $cartRepository;
 
     /**
      * QuoteItemExtractor constructor.
-     * @param \Magento\Quote\Api\CartItemRepositoryInterface $cartItemRepository
+     * @param CartRepositoryInterface $cartRepository
      */
-    public function __construct(\Magento\Quote\Api\CartItemRepositoryInterface $cartItemRepository)
+    public function __construct(CartRepositoryInterface $cartRepository)
     {
-        $this->cartItemRepository = $cartItemRepository;
+        $this->cartRepository = $cartRepository;
     }
 
     /**
@@ -41,11 +42,11 @@ class QuoteItemExtractor
             return $this->items[$quoteId][$itemId];
         }
         try {
-            $cartItems = $this->cartItemRepository->getList($quoteId);
+            $quote = $this->cartRepository->get((int) $quoteId);
         } catch (NoSuchEntityException $e) {
             return null;
         }
-        foreach ($cartItems as $item) {
+        foreach ((array) $quote->getItems() as $item) {
             $this->items[$quoteId][$item->getItemId()] = $item;
         }
         return $this->items[$quoteId][$itemId] ?? null;
